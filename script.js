b15474001646cda5c13dc58924b758a3d78f06c8
VillyRosa <villyrosa@hotmail.com>
2024-01-23 18:00:44 -0300

    fixed numbers passing through display

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function updateDisplay() {
 }
 
 function calcResult() {
-    let res = eval(removerTagsSpan(textDisplay));
+    let res = eval(removeSpanTags(textDisplay));
     if (res === undefined) res = 'error';
     textDisplay = res;
     resultDisplay.innerHTML = res;
@@ -22,21 +22,16 @@ function toggleDarkMode() {
     document.querySelector('svg.backspaceDark').classList.toggle('hidden');
 }
 
-function removerTagsSpan(inputString) {
-    // Cria um elemento temporário para inserir a string e manipular o DOM
+function removeSpanTags(inputString) {
     const tempElement = document.createElement('div');
     tempElement.innerHTML = inputString;
-  
-    // Obtém todas as tags <span> dentro do elemento temporário
     const spans = tempElement.getElementsByTagName('span');
   
-    // Itera sobre as tags <span> e substitui cada uma pelo seu conteúdo interno
     for (let i = spans.length - 1; i >= 0; i--) {
-      const span = spans[i];
-      const spanConteudo = span.innerHTML;
-      span.parentNode.replaceChild(document.createTextNode(spanConteudo), span);
+        const span = spans[i];
+        const spanConteudo = span.innerHTML;
+        span.parentNode.replaceChild(document.createTextNode(spanConteudo), span);
     }
-  
-    // Retorna a string modificada
+
     return tempElement.innerHTML;
 }
